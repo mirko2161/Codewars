6a6bab29361d15dff7daf1429d284044addd6012
mirko <mirko.djurdjevic@ymail.com>
2017-11-04 20:48:09 +0100

    Add Mumbling kata

---
+++ src/kata/Accumul.java
@@ -0,0 +1,45 @@
+package kata;
+
+/**
+ * See https://www.codewars.com/kata/mumbling
+ *
+ * Examples:
+ *
+ * Accumul.accum("abcd"); // "A-Bb-Ccc-Dddd"
+ *
+ * Accumul.accum("RqaEzty"); // "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+ *
+ * Accumul.accum("cwAt"); // "C-Ww-Aaa-Tttt"
+ *
+ * The parameter of accum is a string which includes only letters from a..z and A..Z.
+ *
+ * @author mirko
+ */
+public class Accumul {
+
+    public static String accum(String word) {
+        StringBuffer newWord = new StringBuffer();
+        for (int i = 0; i < word.length(); i++) {
+            if (i != 0) {
+                newWord.append("-");
+            }
+            newWord.append(Character.toUpperCase(word.charAt(i)));
+            for (int j = 0; j < i; j++) {
+                newWord.append(Character.toLowerCase(word.charAt(i)));
+            }
+        }
+        return newWord.toString();
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Fixed Tests accum");
+        assert (Accumul.accum("ZpglnRxqenU").equals("Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"));
+        assert (Accumul.accum("NyffsGeyylB").equals("N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb"));
+        assert (Accumul.accum("MjtkuBovqrU").equals("M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu"));
+        assert (Accumul.accum("EvidjUnokmM").equals("E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm"));
+        assert (Accumul.accum("HbideVbxncC").equals("H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc"));
+
+    }
+
+}
