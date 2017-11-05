4dc346c32ff96557fdd3d1e7d96d6e4f7ee22da8
mirko <mirko.djurdjevic@ymail.com>
2017-11-05 22:13:17 +0100

    Add BouncingBall kata

--- src/kata/HighestScoringWord.java
+++ src/kata/HighestScoringWord.java
@@ -1,7 +1,7 @@
 package kata;
 
 /**
- * See
+ * See https://www.codewars.com/kata/57eb8fcdf670e99d9b000272
  *
  * Given a string of words, you need to find the highest scoring word. Each letter of a word scores
  * points according to it's position in the alphabet: a = 1, b = 2, c = 3 etc. You need to return
---
+++ test/kata/BouncingBallTest.java
@@ -0,0 +1,22 @@
+package kata;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class BouncingBallTest {
+
+    @Test
+    public void test1() {
+        assertEquals(3, BouncingBall.bouncingBall(3.0, 0.66, 1.5));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(15, BouncingBall.bouncingBall(30.0, 0.66, 1.5));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals(-1, BouncingBall.bouncingBall(3.0, 1, 1.5));
+    }
+}
