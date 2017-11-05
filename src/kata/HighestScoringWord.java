9f9af960f79df073a582f04665ef2b85fcbcef13
mirko <mirko.djurdjevic@ymail.com>
2017-11-05 21:32:43 +0100

    Add HighestScoringWord kata

---
+++ src/kata/HighestScoringWord.java
@@ -0,0 +1,34 @@
+package kata;
+
+/**
+ * See
+ *
+ * Given a string of words, you need to find the highest scoring word. Each letter of a word scores
+ * points according to it's position in the alphabet: a = 1, b = 2, c = 3 etc. You need to return
+ * the highest scoring word as a string.
+ *
+ * If two words score the same, return the word that appears earliest in the original string. All
+ * letters will be lowercase and all inputs will be valid.
+ *
+ * @author mirko
+ */
+public class HighestScoringWord {
+
+    public static String high(String stringOfWords) {
+        String highscoreWord = "";
+        int maxTotal = 0;
+
+        for (String word : stringOfWords.split(" ")) {
+            int totalOfWord = 0;
+            for (char c : word.toCharArray()) {
+                totalOfWord += c - 96; // the int value of "a" is 97
+            }
+            if (totalOfWord > maxTotal) {
+                maxTotal = totalOfWord;
+                highscoreWord = word;
+            }
+        }
+        return highscoreWord;
+    }
+
+}
---
+++ test/kata/HighestScoringWordTest.java
@@ -0,0 +1,15 @@
+package kata;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class HighestScoringWordTest {
+
+    @Test
+    public void sampleTests() {
+        assertEquals("taxi", HighestScoringWord.high("man i need a taxi up to ubud"));
+        assertEquals("volcano", HighestScoringWord.high("what time are we climbing up to the volcano"));
+        assertEquals("semynak", HighestScoringWord.high("take me to semynak"));
+    }
+
+}
