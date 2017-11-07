332a459857b541908d76e87e3121fc24fc40d8b3
mirko <mirko.djurdjevic@ymail.com>
2017-11-07 20:42:08 +0100

    Add Dubstep kata

---
+++ test/kata/DubstepTest.java
@@ -0,0 +1,15 @@
+package kata;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class DubstepTest {
+
+    public DubstepTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+
+}
